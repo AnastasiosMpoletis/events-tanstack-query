0464efe5b7fea2afc349bbd98937492f09943f65
AnastasiosMpoletis <anastasiosmpoletis@gmail.com>
2025-06-28 21:49:03 +0300

    L-424. Implement optimistic event updating

--- src/components/Events/EditEvent.jsx
+++ src/components/Events/EditEvent.jsx
@@ -1,6 +1,6 @@
 import { Link, useNavigate, useParams } from 'react-router-dom';
 import { useQuery, useMutation } from '@tanstack/react-query';
-import { fetchEvent, updateEvent } from '../../util/http.js';
+import { fetchEvent, updateEvent, queryClient } from '../../util/http.js';
 
 import Modal from '../UI/Modal.jsx';
 import EventForm from './EventForm.jsx';
@@ -18,6 +18,26 @@ export default function EditEvent() {
 
   const { mutate } = useMutation({
     mutationFn: updateEvent,
+    /**
+     * It is called the moment we call mutate.
+     * @param {*} data is passed automatically by onMutate
+     */
+    onMutate: async (data) => {
+      // we cancel any ongoing queries to avoid clash problems
+      await queryClient.cancelQueries({ queryKey: ['events', params.id] });
+      const previousEvent = queryClient.getQueryData(['events', params.id]);
+      queryClient.setQueryData(['events', params.id], data.event); // manipulates stored data
+
+      return { previousEvent }; // this object will be passed to onError's context
+    },
+    // called if mutationFn fails
+    onError: (error, data, context) => {
+      queryClient.setQueryData(['events', params.id], context.previousEvent);
+    },
+    // called when mutationFn is finished, no matter if it failed or succeeded. We use it as a failsafe in order to have valid data in the end
+    onSettled: () => {
+      queryClient.invalidateQueries(['events', params.id]);
+    },
   });
 
   function handleSubmit(formData) {
